d8fed417fffdd104790e8199ca9c7d599f1057e3
Manikanta-Kummarapurugu <125174814+Manikanta-Kummarapurugu@users.noreply.github.com>
2025-06-09 10:42:52 +0530

    commit

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -6,25 +6,44 @@ import { Component } from '@angular/core';
   imports: [CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
-  styles: `
-        @media screen and (max-width: 991px) {
-            .video-container {
-                position: relative;
-                width: 100%;
-                height: 0;
-                padding-bottom: 56.25%;
-
-                iframe {
-                    position: absolute;
-                    top: 0;
-                    left: 0;
-                    width: 100%;
-                    height: 100%;
-                }
-            }
-        }
-    `
+  
 })
 export class DashboardComponent {
-
+ stats = [
+    {
+      label: 'Messages',
+      count: 152,
+      new: 24,
+      icon: 'pi pi-envelope',
+      gradientFrom: 'from-cyan-400 dark:from-cyan-300',
+      gradientTo: 'to-cyan-600 dark:to-cyan-500',
+      subtext: 'since last visit',
+    },
+    {
+      label: 'Check-ins',
+      count: 532,
+      new: 48,
+      icon: 'pi pi-map-marker',
+      gradientFrom: 'from-orange-400 dark:from-orange-300',
+      gradientTo: 'to-orange-600 dark:to-orange-500',
+      subtext: 'since last visit',
+    },
+    {
+      label: 'Files Synced',
+      count: '28.441',
+      usage: '32,56 / 250 GB',
+      icon: 'pi pi-file',
+      gradientFrom: 'from-slate-400 dark:from-slate-300',
+      gradientTo: 'to-slate-600 dark:to-slate-500',
+    },
+    {
+      label: 'Users Online',
+      count: '25.660',
+      new: 72,
+      icon: 'pi pi-users',
+      gradientFrom: 'from-violet-400 dark:from-violet-300',
+      gradientTo: 'to-violet-600 dark:to-violet-500',
+      subtext: 'user this week',
+    },
+  ];
 }
